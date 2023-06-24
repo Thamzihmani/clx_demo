9b5d501b4c03f054eeb12877e59dbdefc936248b
ThamizhmaniC <thamizhmani0@gmail.com>
2023-06-24 20:03:40 +0530

    The Task completed as per the Discussion

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:clx_demo/views/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
